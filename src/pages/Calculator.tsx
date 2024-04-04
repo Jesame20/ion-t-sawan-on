3a182ad46e523a943b198c91e7d4fd03dc4a075a
Jesame20 <jcsawanonshs@gmail.com>
2024-04-04 21:57:25 +0800

    Added code on Calculator.tsx

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { IonGrid, IonRow, IonCol, IonButton, IonInput } from '@ionic/react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(input);
+      setInput(result.toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  return (
+    <IonGrid className="calculator">
+      <IonRow>
+        <IonCol size="12">
+          <IonInput className="display" value={input} readonly></IonInput>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol size="3">
+          <IonButton className="btn" onClick={() => handleClick('7')}>7</IonButton>
+        </IonCol>
+        <IonCol size="3">
+          <IonButton className="btn" onClick={() => handleClick('8')}>8</IonButton>
+        </IonCol>
+        <IonCol size="3">
+          <IonButton className="btn" onClick={() => handleClick('9')}>9</IonButton>
+        </IonCol>
+        <IonCol size="3">
+          <IonButton className="operator" onClick={() => handleClick('/')}>/</IonButton>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        {/* Other buttons */}
+      </IonRow>
+      <IonRow>
+        {/* Other rows of buttons */}
+      </IonRow>
+      <IonRow>
+        {/* Equal and Clear buttons */}
+      </IonRow>
+    </IonGrid>
+  );
+};
+
+export default Calculator;
