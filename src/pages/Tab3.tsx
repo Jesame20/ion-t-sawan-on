151a8aa4fd749f916000dcefffa9ee0a1cf34a19
Jesame20 <jcsawanonshs@gmail.com>
2024-04-04 21:52:31 +0800

    Imported Ionic components on Tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import React, { useState } from 'react';
+import { IonGrid, IonRow, IonCol, IonButton, IonInput } from '@ionic/react';
+import './Calculator.css';
 
 const Tab3: React.FC = () => {
   return (
