3f3ec21e9bd670ff8cc394825f07d98920524bef
Jesame20 <jcsawanonshs@gmail.com>
2024-04-14 00:14:09 +0800

    Added structure on clickcounter.tsx

--- src/pages/Clickcounter.tsx
+++ src/pages/Clickcounter.tsx
@@ -1,35 +1,47 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton} from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCard, IonCardContent, IonContent, IonPage } from '@ionic/react';
 import './Clickcounter.css';
 
-const Clickcounter: React.FC = () => {
-  const [clickCount, setClickCount] = useState(0);
+const ClickCounter: React.FC = () => {
+    // State variable to keep track of the click count
+    const [clickCount, setClickCount] = useState(0);
 
-  const incrementClickCount = () => {
-    setClickCount(clickCount + 1);
-  };
+    // Function to reset the click counter
+    const resetClickCount = () => {
+        setClickCount(0);
+    };
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Click Counter</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <div>
-          <IonButton onClick={incrementClickCount}>Click Me</IonButton>
-          <p>Click Count: {clickCount}</p>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+                <IonCard>
+                    <IonCardContent className="click-counter">
+                        {/* Display click count and reset button side by side */}
+                        <div className="click-count-reset-container">
+                            <div className="click-counter-text">Click Count: {clickCount}</div>
+                            <IonButton
+                                onClick={resetClickCount}
+                                color="secondary"
+                                className="reset-button"
+                            >
+                                Reset Counter
+                            </IonButton>
+                        </div>
+
+                        {/* Button that increments click count */}
+                        <IonButton
+                            onClick={() => setClickCount(clickCount + 1)}
+                            color="primary"
+                            expand="full"
+                            className="click-button"
+                        >
+                            Click Me
+                        </IonButton>
+                    </IonCardContent>
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    );
 };
 
-export default Clickcounter;
+export default ClickCounter;
