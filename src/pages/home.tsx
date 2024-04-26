755b89ef1dab07951a28be283c879c09247193b6
Jesame20 <jcsawanonshs@gmail.com>
2024-04-26 08:34:36 +0800

    Added a dynamic search on home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,67 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardContent,  
-    IonCardSubtitle, IonCardTitle, IonCard, IonList, IonItem,IonThumbnail, IonLabel
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardContent,
+  IonCardSubtitle, IonCardTitle, IonCard, IonList, IonItem, IonThumbnail, IonLabel,
+  IonSearchbar
 } from '@ionic/react';
 import { IonRouterLink } from '@ionic/react';
 import './home.css';
 
+const cardData = [
+  {
+    title: 'Click Counter',
+    icon: 'https://img.freepik.com/premium-vector/click-arrow-touch-point-logo-template-design-vector-emblem-design-concept-creative-symbol_20029-363.jpg',
+    subtitle: 'Applet #1',
+    link: '/Clickcounter'
+  },
+  {
+    title: 'Calculator',
+    icon: 'https://cdn-icons-png.flaticon.com/512/1011/1011863.png',
+    subtitle: 'Applet #2',
+    link: '/Calculator'
+  },
+  {
+    title: 'Todo List',
+    icon: 'https://cdn-icons-png.flaticon.com/512/7692/7692809.png',
+    subtitle: 'Applet #3',
+    link: '/Todolist'
+  },
+  {
+    title: 'Blank',
+    icon: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    subtitle: 'Applet #4',
+    link: ''
+  }
+];
 
-  const Home: React.FC = () => {
-    return (
-      <IonPage className="general-background">
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+const Home: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  return (
+    <IonPage className="general-background">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <div className="home-container">
-      <header>
-        <h1>Welcome to My App</h1>
-        <p>Your one-stop solution for all your needs.</p>
-      </header>
-      <IonCard>
-      <IonCardContent>
-        <IonList>
-        <IonRouterLink href="/Clickcounter">
-              <IonItem>
-                  <IonThumbnail slot="start">
-                    <img alt="Silhouette of mountains" src="https://img.freepik.com/premium-vector/click-arrow-touch-point-logo-template-design-vector-emblem-design-concept-creative-symbol_20029-363.jpg" />
-                  </IonThumbnail>
-                  <IonLabel>Click Counter</IonLabel>
-              </IonItem>
-                </IonRouterLink>
-              
-              <IonRouterLink href="/Calculator">
-              <IonItem>
-                <IonThumbnail slot="start">
-                    <img id="img" alt="Silhouette of mountains" src="https://cdn-icons-png.flaticon.com/512/1011/1011863.png" />
-                  </IonThumbnail>
-                  <IonLabel>Calculator</IonLabel>
-              </IonItem>
-                </IonRouterLink >
-
-                <IonRouterLink href="/Todolist">
-              <IonItem>
-                <IonThumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://cdn-icons-png.flaticon.com/512/7692/7692809.png" />
-                </IonThumbnail>
-                <IonLabel>Todo List</IonLabel>
-              </IonItem>
-              </IonRouterLink >
-              <IonItem lines="none">
-                <IonThumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                </IonThumbnail>
-                <IonLabel>Blank</IonLabel>
-              </IonItem>
-        </IonList>
-      </IonCardContent>
-    </IonCard>
-    </div>
-    </IonContent>
-   </IonPage>
+          <header>
+            <h1>Welcome to My App</h1>
+            <p>Your one-stop solution for all your needs.</p>
+          </header>
+          <IonCard>
+            <IonCardContent>
+              <IonSearchbar
+                value={searchTerm}
+                onIonInput={(e) => setSearchTerm(e.target.value ?? '')}
+              />
+              <IonList>
+                {cardData
+                  .filter((card) => card.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                  .map((card, index) => (
+                    <IonRouterLink key={index} href={card.link}>
+                      <IonItem>
+                        <IonThumbnail slot="start">
+                          <img alt="Silhouette of mountains" src={card.icon} />
+                        </IonThumbnail>
+                        <IonLabel>{card.title}</IonLabel>
+                      </IonItem>
+                    </IonRouterLink>
+                ))}
+              </IonList>
+            </IonCardContent>
+          </IonCard>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
-  export default Home;
+
+export default Home;
