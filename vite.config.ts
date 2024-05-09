eff94da9ffae4d878028b246f6c6c093170c9d32
Jesame20 <jcsawanonshs@gmail.com>
2024-05-09 20:30:22 +0800

    Added deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Jesame20/ionic-t-sawan-on/',
   plugins: [
     react(),
     legacy()
