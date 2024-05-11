47a49d80a2b787ee36ba312337b780bb1d47bb59
Jesame20 <jcsawanonshs@gmail.com>
2024-05-11 09:44:27 +0800

    Updates

--- assets/ios.transition-legacy-BOnaexnz.js
+++ assets/ios.transition-legacy-BOnaexnz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DJGF8WH5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.u,n=t.v}],execute:function(){
+System.register(["./index-legacy-Uk4F3TdU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
