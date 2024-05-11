80edf142a0ca965e8179aa9175fca2cbd00be4db
Jesame20 <jcsawanonshs@gmail.com>
2024-05-11 10:25:59 +0800

    Updates

--- assets/ios.transition-legacy-BG5AbdNc.js
+++ assets/ios.transition-legacy-BG5AbdNc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dd4WrBvU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-CVSiIAc-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.u,n=t.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-CE_YBkgd.js
+++ assets/status-tap-legacy-CE_YBkgd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dd4WrBvU.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-CVSiIAc-.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
