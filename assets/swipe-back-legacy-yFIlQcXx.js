90b5249252613a644d41ad9bd7c5b760ba99d1c8
dianemorales <dianemorales330@gmail.com>
2024-05-21 08:50:10 +0800

    Updates

--- assets/swipe-back-legacy-yFIlQcXx.js
+++ assets/swipe-back-legacy-yFIlQcXx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CVSiIAc-.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CJnNhApH.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
